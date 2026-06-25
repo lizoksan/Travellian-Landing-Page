626e2dfbe2452067b3f4ed85ee52375b2e34b839
lizoksan <lizoksan@gmail.com>
2026-06-25 22:19:20 +0300

    fix(planners): reset swiper transform and update styles on mobile

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,64 @@
 import '../styles/main.scss';
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 window.addEventListener('load', () => {
-	console.log('test');
+	const baseSwiperConfig = {
+		modules: [Navigation],
+		direction: 'horizontal',
+		loop: true,
+		grabCursor: true,
+		initialSlide: 1,
+		freeMode: true,
+		scrollbar: false,
+		spaceBetween: 16,
+		slidesPerView: 1,
+		watchSlidesProgress: true,
+
+		breakpoints: {
+			1440: {
+				slidesPerView: 2.7,
+				spaceBetween: 32,
+			},
+		},
+	};
+
+	const swiperDest = new Swiper('.destinations__swiper', {
+		...baseSwiperConfig,
+		navigation: {
+			nextEl: '.destinations__next',
+			prevEl: '.destinations__prev',
+		},
+	});
+
+	const swiperOffers = new Swiper('.offers__swiper', {
+		...baseSwiperConfig,
+		navigation: {
+			nextEl: '.offers__next',
+			prevEl: '.offers__prev',
+		},
+	});
+
+	const swiperReviews = new Swiper('.reviews__swiper', {
+		...baseSwiperConfig,
+		navigation: {
+			nextEl: '.reviews__next',
+			prevEl: '.reviews__prev',
+		},
+	});
+
+	const swiperPlanners = new Swiper('.planners__swiper', {
+		...baseSwiperConfig,
+		enabled: false,
+
+		breakpoints: {
+			1440: {
+				enabled: true,
+				slidesPerView: 2.7,
+				spaceBetween: 32,
+			},
+		},
+	});
 });
